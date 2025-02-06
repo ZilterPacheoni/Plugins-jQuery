fda851b90764721bcff6379b6885fc49785e9246
ZilterPacheoni <ziltermadrats@gmail.com>
2025-02-06 17:24:25 -0300

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+    $('#carouselImagens').slick({
+        autoplay:true
+    });
+
+    $('#telefone').mask('(00) 00000-0000', {
+        placeholder: '(DDD) 1 2345-6789'
+    })
+
+    $('#cpf').mask('000.000.000-00', {
+        placeholder: '123.456.789-00'
+    })
+
+    $('#cep').mask('00000-000', {
+        placeholder: '012345-678'
+    })
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            endereco: {
+                required: true
+            },
+            cep: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
+        },
+
+        submitHandler: function(form) {
+            console.log(form)
+        },
+        invalidHandler: function (evento, validator) {
+            let camposIncorretos = validator.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Por favor, preencha os campos para prosseguir com a compra!`)
+            }
+        }
+    })
+})
